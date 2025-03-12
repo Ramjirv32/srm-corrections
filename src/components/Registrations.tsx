72fcd22ad18d05ca7fbcabb17c2d8f986afcf955
ramji <ramjib2311@gmail.com>
2025-03-12 13:48:02 +0530

    last-error

--- src/components/Registrations.tsx
+++ src/components/Registrations.tsx
@@ -34,11 +34,11 @@ const Registrations: React.FC = () => {
   const bankDetailsRef = useRef<HTMLDivElement>(null);
   
   // eslint-disable-next-line no-unused-vars
-  const handleCopy = (text: string, field: string) => {
-    navigator.clipboard.writeText(text);
-    setCopiedField(field);
-    setTimeout(() => setCopiedField(null), 2000);
-  };
+  // const handleCopy = (text: string, field: string) => {
+  //   navigator.clipboard.writeText(text);
+  //   setCopiedField(field);
+  //   setTimeout(() => setCopiedField(null), 2000);
+  // };
   
   const copyAllBankDetails = () => {
     if (bankDetailsRef.current) {
